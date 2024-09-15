71c1838f8f51b5d856dcd9914b5748aa42838114
Prashant Gaware <prashantgaware8@gmail.com>
2024-09-15 21:47:10 +0530

    BootProj05-MiniProject-Profile

---
+++ BootProj05-MiniProject-Profile/src/main/java/com/nt/BootProj05MiniProjectProfileApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj05MiniProjectProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj05MiniProjectProfileApplication.class, args);
+	}
+
+}
